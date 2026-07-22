ae744378a9ceea432fa1ee512e5b9ecd353d3077
psdmcc <psdmcc@users.noreply.github.com>
2026-07-22 15:45:57 +0900

    data: update tracking patterns with Gethsemane ballistic unction variables

--- analyze_echidna_matrix.py
+++ analyze_echidna_matrix.py
@@ -20,7 +20,40 @@ def run_expanded_matrix_analysis(raw_texts_dir="raw_texts"):
         'Soterion_Pharmakon': re.compile(r"\b(σωτηρ[-]{0,2}|σωτερ[-]{0,2}|φαρμακ[-]{0,2})", re.IGNORECASE | re.UNICODE),
         'Evangelion_Efficacy': re.compile(r"\b(εὐαγγελ[-]{0,2}|ευαγγελ[-]{0,2})", re.IGNORECASE | re.UNICODE),
         'Christos_Compound': re.compile(r"\b(χριστ[-]{0,2}|χρισ[-]{0,2}|χρισμ[-]{0,2}|χριω|χριο)", re.IGNORECASE | re.UNICODE)
-    }
+        "purple_dye": re.compile(r"\bπορφυρ(?:α|ᾱ|ας|ᾳ|αν|ων|αις|ες|ο|οῦ|ᾶς|οῦν|οῖ)\w*\b", re.IGNORECASE),
+        "drakaina": re.compile(r"\bδρακαιν(?:α|ης|ῃ|αν|αι|ων|αις)\b", re.IGNORECASE),
+        "gypsum": re.compile(r"\bγυψ(?:ος|ου|ῳ|ον|ε|ων|οις|ους)\b", re.IGNORECASE),
+        "theriake_vessel": re.compile(r"\bθηριακ\w*\b", re.IGNORECASE),
+        "galene_stabilization": re.compile(r"\bγαλην(?:η|ης|ῃ|αν|αι|ων|αις)\b", re.IGNORECASE),
+        "dialysis_neutralization": re.compile(r"\bδιαλυ(?:ω|εις|ει|ομεν|ετε|ουσι|ειν|ης|η|ον)\b", re.IGNORECASE),
+        "somatic_ingestion": re.compile(r"\b(αἱμα|σαρκ|ἑψειν|πινειν)\w*\b", re.IGNORECASE),
+        "heresiological_synthesis": re.compile(r"\b(μανιχαι|συντιθε|αθανασι|αμβροσι)\w*\b", re.IGNORECASE),
+        "byzantine_translatio": re.compile(r"\b(εχιδνολογ|σαρκ|ἑρπιστικ|σωτηρι)\w*\b", re.IGNORECASE),
+        "generic_serpent": re.compile(r"\bοφι(?:ς|ος|ῳ|ον|ε|ων|οις|ους)\b", re.IGNORECASE),
+        "dipsas_dehydration": re.compile(r"\bδιψα\w*\b", re.IGNORECASE),
+        "synchrisma_ointment": re.compile(r"\bσυγχρισμ\w*\b", re.IGNORECASE),
+        "draconic_agent": re.compile(r"\bδρακοντ(?:ος|ῳ|α|ες|ων|οις|ους)\b", re.IGNORECASE),
+        "potable_vehicle": re.compile(r"\b(ποθεισ|πινω|οινω)\w*\b", re.IGNORECASE),
+        "corporate_tech_transfer": re.compile(r"\b(χριομεν|σαρκων|σκευασια|αντιδοτ)\w*\b", re.IGNORECASE),
+        "gnostic_pushback": re.compile(r"\b(ληστ|ληστρικ|μανιχ|ναγ[-_]χαμμ)\w*\b", re.IGNORECASE),
+        "scythian_ballistics": re.compile(r"\b(σκυθικ|οϊστου|οιστο)\w*\b", re.IGNORECASE),
+        "ballistic_chrisma": re.compile(r"\b(χρισμα_βελ|χριουσιν|αβαριδ)\w*\b", re.IGNORECASE),
+        "theriake_vessel": re.compile(r"\bθηριακ\w*\b", re.IGNORECASE),
+        "galene_stabilization": re.compile(r"\bγαλην(?:η|ης|ῃ|αν|αι|ων|αις)\b", re.IGNORECASE),
+        "dialysis_neutralization": re.compile(r"\bδιαλυ(?:ω|εις|ει|ομεν|ετε|ουσι|ειν|ης|η|ον)\b", re.IGNORECASE),
+        "somatic_ingestion": re.compile(r"\b(αἱμα|σαρκ|ἑψειν|πινειν)\w*\b", re.IGNORECASE),
+        "heresiological_synthesis": re.compile(r"\b(μανιχαι|συντιθε|αθανασι|αμβροσι)\w*\b", re.IGNORECASE),
+        "byzantine_translatio": re.compile(r"\b(εχιδνολογ|σαρκ|ἑρπιστικ|σωτηρι)\w*\b", re.IGNORECASE),
+        "generic_serpent": re.compile(r"\bοφι(?:ς|ος|ῳ|ον|ε|ων|οις|ους)\b", re.IGNORECASE),
+        "dipsas_dehydration": re.compile(r"\bδιψα\w*\b", re.IGNORECASE),
+        "synchrisma_ointment": re.compile(r"\bσυγχρισμ\w*\b", re.IGNORECASE),
+        "draconic_agent": re.compile(r"\bδρακοντ(?:ος|ῳ|α|ες|ων|οις|ους)\b", re.IGNORECASE),
+        "potable_vehicle": re.compile(r"\b(ποθεισ|πινω|οινω)\w*\b", re.IGNORECASE),
+        "corporate_tech_transfer": re.compile(r"\b(χριομεν|σαρκων|σκευασια|αντιδοτ)\w*\b", re.IGNORECASE),
+        "gnostic_pushback": re.compile(r"\b(ληστ|ληστρικ|μανιχ|ναγ[-_]χαμμ)\w*\b", re.IGNORECASE),
+        "scythian_ballistics": re.compile(r"\b(σκυθικ|οϊστου|οιστο)\w*\b", re.IGNORECASE),
+        "ballistic_chrisma": re.compile(r"\b(χρισμα_βελ|χριουσιν|αβαριδ)\w*\b", re.IGNORECASE)
+}
 
     print(f"[*] Sweeping 2,491 assets for the Expanded Pharmako-Mantis Matrix...")
     
